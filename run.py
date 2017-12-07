8108d2269967eeac218cce55364b6983f7c5d3dd
Basemera <bsmrrachel@gmail.com>
2017-12-07 19:06:36 +0300

    add the run.py

---
+++ run.py
@@ -0,0 +1,13 @@
+#run.py
+
+import os
+
+from app import create_app
+from instance.config import app_config
+
+config_name = 'development'
+#config_name = os.getenv('APP_SETTINGS') # config_name = "development"
+app = create_app(config_name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
