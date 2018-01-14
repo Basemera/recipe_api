5b77b1f7008b713d61042fd980aafcc4db688a0b
Basemera <phiona.basemera@andela.com>
2018-01-14 15:14:56 +0300

    adding the instance folder

---
+++ instance/config.py
@@ -0,0 +1,37 @@
+import os
+
+class Config(object):
+    """Parent configuration class."""
+    DEBUG = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'xcEN1Sbcp39XKraZVytFEzDJdKVDZZRg'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost:5432/recipe_api'
+
+
+class DevelopmentConfig(Config):
+    """Configurations for Development."""
+    DEBUG = True
+
+class TestingConfig(Config):
+    """Configurations for Testing, with a separate test database."""
+    TESTING = True
+    # global SQLALCHEMY_DATABASE_URI
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost:5432/test_db'
+    DEBUG = True
+
+class StagingConfig(Config):
+    """Configurations for Staging."""
+    DEBUG = True
+
+class ProductionConfig(Config):
+    """Configurations for Production."""
+    DEBUG = False
+    TESTING = False
+
+app_config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'staging': StagingConfig,
+    'production': ProductionConfig,
+}
