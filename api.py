000f498f28014236216453287467e3f73be2ea8b
Basemera <phiona.basemera@andela.com>
2018-03-13 19:08:17 +0300

    Add bluePrints

--- api.py
+++ api.py
@@ -1,6 +1,15 @@
 import os
 
+from flask_restful import Resource, Api
 from api_recipe import create_app
+from api_recipe.auth import autho
+from api_recipe.categories import category
+from api_recipe.recipes import recipe
 
+api = Api()
 config_name = os.environ.get('APP_SETTINGS')
 app = create_app(config_name)
+api.init_app(app)
+app.register_blueprint(autho)
+app.register_blueprint(category)
+app.register_blueprint(recipe)
