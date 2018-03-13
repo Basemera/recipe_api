b3eb47c0acda48939a192ce6c1e1361971aee8c5
Basemera <phiona.basemera@andela.com>
2018-03-13 17:42:17 +0300

    Add routes

--- instance/config.py
+++ instance/config.py
@@ -3,7 +3,9 @@ from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
 from flask_httpauth import HTTPBasicAuth
 from flask_restful import Resource, Api
-
+from api_recipe.auth import autho
+from api_recipe.categories import category
+from api_recipe.recipes import recipe
 
 class Config(object):
     """Parent configuration class."""
@@ -54,4 +56,7 @@ def create_app(config_name):
     db.app = app
     db.init_app(app)
     api.init_app(app)
+    app.register_blueprint(autho)
+    app.register_blueprint(category)
+    app.register_blueprint(recipe)
     return app
